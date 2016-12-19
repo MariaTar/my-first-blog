63d1b93280fd074d3d16e2593392c100e30ba200
Maria <maria.tarasiuck@gmail.com>
2016-12-19 13:47:58 +0200

    added view and template for post

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e!qrbdn4a66!y*yo3u**e79)!o_eubs=8j8!h_3(ga42_%c!i#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
